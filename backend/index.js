cf250a8f2a62906769c453df4edbdca39fe37f99
Dhruva <dhruvatajapara@gmail.com>
2026-04-07 13:11:02 +0530

    fix error

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const connectDB = require("./config/db");
 const pincodeRoutes = require("./routes/pincodeRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(cors());
